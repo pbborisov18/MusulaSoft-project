bc0e89e3c31f290053e4437553038c7c19dfe064
pbborisov18 <56866287+pbborisov18@users.noreply.github.com>
2021-02-15 19:53:50 +0200

    Main Functions added

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void studentAdd();      //fuction which adds students to the database
+void teacherAdd();      //function which adds teams to the database
+void teamsAdd();        //function which adds teachers to the database
+void studentsEdit();    //function which edits students in the database
+void teamsEdit();       //function which edits teams in the database
+void teachersEdit();    //function which edits teachers in the database
+void studentsDelete();  //function which deletes students off the database
+void teamsDelete();     //function which deletes teams off the database
+void teachersDelete();  //function which deletes teachers off the database
+void searchTeams();     //search function for teams
+void searchNames();     //search function for names
+void mainMenu();        //function main menu
+
+
+int main()
+{
+    return 0;
+}
