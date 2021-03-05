58da6f78e104ad20ead968e9a19831add3da6400
pbborisov18 <56866287+pbborisov18@users.noreply.github.com>
2021-03-05 20:43:26 +0200

    Added information on how the functions work

--- main.cpp
+++ main.cpp
@@ -1,26 +1,47 @@
 #include <iostream>
-#include <iostream>
-#include <fstream>
+#include <vector>
 #include <string>
-#include <stdio.h>
-
 using namespace std;
 
-void studentsAdd();     //function which adds students to Students.csv                      done
-void teachersAdd();      //function which adds teachers to Teachers.csv                     done
-void teamsAdd();        //function which adds teams to Teams.csv                            done
-void studentsDelete();  //function which deletes students off Students.csv                  done (No connections)
-void teachersDelete();  //function which deletes teachers off Teachers.csv                  done (No connections)
-void teamsDelete();     //function which deletes teams off Teams.csv
+/* Students menu*/
+void studentsAdd(); //function which adds students to Students.csv
+
+void studentsDelete();  //function which deletes students off Students.csv
+void connectionsRemoveStudentFromTeam(string studentEmail); //connections from Students.csv to Teams.csv
+
 void studentsEdit();    //function which edits students information in Students.csv
+
+/* Teachers menu*/
+void teachersAdd(); //function which adds teachers to Teachers.csv
+
+void teachersDelete();  //function which deletes teachers off Teachers.csv
+void connectionsRemoveTeacherFromTeam(string teacherEmail); //connections from Teachers.csv to Teams.csv
+
 void teachersEdit();    //function which edits teachers information in Teachers.csv
-void teamsEdit();       //function which edits teams information in Teams.csv
-void searchStudents();  //search function for students                                      done
-void searchTeachers();  //search function for teachers                                      done
-void searchTeams();     //search function for teams                                         done
-void searchStudentsTm();//search function for students in teams menu
-void searchTeachersTm();//search function for teachers in teams menu
-void mainMenu();        //function main menu
+
+/* Teams menu*/
+void teamsAdd();    //function which adds teams to Teams.csv
+vector<string> searchStudentTeam(string searchTermS);   //connections from Teams.csv to Students.csv Used for displaying information to the user and checking if student is legible to be in a team
+vector<string> searchTeacherTeam(string searchTermT);   //connections from Teams.csv to Teachers.csv Used for displaying information to the user
+void connectionsChangeStudentTeam(string studentName,string studentTeam);   //connections from Teams.csv to Students.csv Used for changing students team in Students.csv
+void connectionsChangeTeacherTeam(string teacherEmail); //connections from Teams.csv to Teachers.csv Used for changing the number of teams the teacher is part in
+
+void teamsDelete(); //function which deletes teams off Teams.csv
+void connectionsChangeStudentTeamToNo(string teamName); //connection from Teams.csv to Students.csv Used for changing the team to No after deletion of the team
+void connectionsChangeTeacherTeamDown(string teacherEmail); //connection from Teams.csv to Teachers.csv Used for changing the number of teams the teacher is part in
+
+void teamsEdit();   //function which edits teams information in Teams.csv
+
+/* Search menu*/
+void searchStudents();  //search function for students
+
+void searchTeachers();  //search function for teachers
+
+void searchTeams(); //search function for teams
+
+
+
+void mainMenu();
 
 int main()
 {
